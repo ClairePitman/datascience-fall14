655db95154eeab01daaeb99b4a9da893dd4e3fce
Claire Pitman <cpitman@terpmail.umd.edu>
2014-11-20 18:36:18 -0500

    .

--- lab6/WordCount.java
+++ lab6/WordCount.java
@@ -1,6 +1,7 @@
 package org.myorg;
 
 	import java.io.IOException;
+	import java.io.*;
 	import java.util.*;
 	
 	import org.apache.hadoop.fs.Path;
@@ -14,16 +15,23 @@ package org.myorg;
         public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
             private final static IntWritable one = new IntWritable(1);
             private Text word = new Text();
+	    private Text preword = new Text();
+	    private Text space = new Text(" ");
 
             public void map(LongWritable key, Text value, OutputCollector output, Reporter reporter) throws IOException {
                 String line = value.toString();
                 StringTokenizer tokenizer = new StringTokenizer(line);
                 while (tokenizer.hasMoreTokens()) {
+		    preword.set(word);
                     word.set(tokenizer.nextToken());
-                    output.collect(word, one);
+		    preword.append(space.getBytes(),0,space.getLength());
+		    preword.append(word.getBytes(),0,word.getLength());
+                    output.collect(preword, one);
                 }
             }
         }
+	
+	
 	
         public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
             public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
@@ -32,6 +40,52 @@ package org.myorg;
                     sum += values.next().get();
                 }
                 output.collect(key, new IntWritable(sum));
+            }
+        }
+	
+	public static class Map2 extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+            private final static IntWritable one = new IntWritable(1);
+            private Text word1 = new Text();
+	    private Text word2 = new Text();
+	    private String count ="0";
+
+            public void map(LongWritable key, Text value, OutputCollector output, Reporter reporter) throws IOException {
+                String line = value.toString();
+                StringTokenizer tokenizer = new StringTokenizer(line);
+		if(tokenizer.hasMoreTokens())
+		word1.set(tokenizer.nextToken());
+		if(tokenizer.hasMoreTokens())
+		word2.set(tokenizer.nextToken());
+		if(tokenizer.hasMoreTokens())
+		count=tokenizer.nextToken();
+		String gram=word1.toString()+" "+word2.toString();
+               	output.collect(word1,new Text(count+" "+gram));
+		output.collect(word2,new Text(count+" "+gram));
+		//output.collect(word1,new IntWritable(Integer.parseInt(count)));
+		//output.collect(word2,new IntWritable(Integer.parseInt(count)));
+            }
+        }
+	
+        public static class Reduce2 extends MapReduceBase implements Reducer<Text, Text, Text, Text> {
+            public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+                
+		int max = 0;
+		Text maxbi=null;
+		Text temp;
+                while (values.hasNext()) {
+                    temp= values.next();
+		    StringTokenizer tokenizer = new StringTokenizer(temp.toString());
+		    if(tokenizer.hasMoreTokens()){
+		    int i=Integer.parseInt(tokenizer.nextToken());
+		    if (i>max){
+		    max=i;
+		    maxbi=temp;
+		    }
+		    
+		    }
+                }
+		if(maxbi!=null)
+                output.collect(key, maxbi);
             }
         }
 	
@@ -50,9 +104,29 @@ package org.myorg;
 	     conf.setOutputFormat(TextOutputFormat.class);
 	
 	     FileInputFormat.setInputPaths(conf, new Path(args[0]));
-	     FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+	     FileOutputFormat.setOutputPath(conf, new Path("wordcount/tmp"));
 	
 	     JobClient.runJob(conf);
+	     
+	     
+	     JobConf conf2 = new JobConf(WordCount.class);
+	     conf2.setJobName("wordcountp2");
+	
+	     conf2.setOutputKeyClass(Text.class);
+	     conf2.setOutputValueClass(Text.class);
+	
+	     conf2.setMapperClass(Map2.class);
+	     conf2.setCombinerClass(Reduce2.class);
+	     conf2.setReducerClass(Reduce2.class);
+	
+	     conf2.setInputFormat(TextInputFormat.class);
+	     conf2.setOutputFormat(TextOutputFormat.class);
+	
+	     FileInputFormat.setInputPaths(conf2, new Path("wordcount/tmp"));
+	     FileOutputFormat.setOutputPath(conf2, new Path(args[1]));
+	     JobClient.runJob(conf2);
 	   }
 	}
+	
+
 	
--- lab6/wordcount.py
+++ lab6/wordcount.py
@@ -8,6 +8,6 @@ textFile = sc.textFile("bible+shakes.nopunc")
 
 counts = textFile.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
 
-print counts.sort().take(100)
+#print counts.sort().take(100)
 
 counts.saveAsTextFile("output")
